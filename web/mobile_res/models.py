602595469d069349178ef7d6c4f257df33cc0c05
Vichoko <vicenteoyanedel@ing.uchile.cl>
2018-03-28 14:31:47 -0300

    Agregada nueva app 'mobile_res' al proyecto (settings) Avances en modelo de datos. Creacion de bateria de test para el modelo de datos.

--- web/mobile_res/models.py
+++ web/mobile_res/models.py
@@ -1,3 +1,32 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Coordinates(models.Model):
+    """
+    latitude and longitude specified in Decimal Degrees (xxx.ddddd)
+    elevation specified in Meters above sea.
+    """
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    elevation = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return "{}, {} [{} m]".format(self.latitude, self.longitude, self.elevation) if self.elevation else "{}, {}".format(self.latitude, self.longitude)
+
+
+class Report(models.Model):
+    """
+    report submitted by a mobile app user.
+    """
+    timestamp = models.DateTimeField(auto_now=True)
+    coordinates = models.ForeignKey(Coordinates, on_delete=models.PROTECT)
+    intensity = models.IntegerField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.intensity < 0 or self.intensity > 12:
+            raise ValidationError("intensity out of range, given {}".format(self.intensity))
+        super().save(*args, **kwargs)
+
+
+
--- web/mobile_res/tests.py
+++ web/mobile_res/tests.py
@@ -1,3 +1,135 @@
+from django.core.exceptions import ValidationError
+from django.db.models import ProtectedError
 from django.test import TestCase
 
-# Create your tests here.
+from mobile_res.models import Coordinates, Report
+
+
+class ModelsTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # fully defined coords
+        cls.full_coord1 = Coordinates.objects.create(
+            latitude=-179.555555,
+            longitude=-122.123456,
+            elevation=8000
+        )
+        cls.full_coord2 = Coordinates.objects.create(
+            latitude=179.555555,
+            longitude=122.123456,
+            elevation=120.5
+        )
+        cls.full_coord3 = Coordinates.objects.create(
+            latitude=-179.555555,
+            longitude=122.123456,
+            elevation=-100.333333333
+        )
+        cls.full_coord4 = Coordinates.objects.create(
+            latitude=-2,
+            longitude=5,
+            elevation=0
+        )
+
+        # parcially defined coords
+        cls.parc_coord1 = Coordinates.objects.create(
+            latitude=-179.555555,
+            longitude=-122.123456
+        )
+        cls.parc_coord2 = Coordinates.objects.create(
+            latitude=179.555555,
+            longitude=122.123456,
+        )
+        cls.parc_coord3 = Coordinates.objects.create(
+            latitude=-179.555555,
+            longitude=122.123456
+        )
+        cls.parc_coord4 = Coordinates.objects.create(
+            latitude=-2,
+            longitude=5
+        )
+
+        # reports
+        cls.report1 = Report.objects.create(coordinates=cls.full_coord1,
+                                            intensity=1)
+
+        cls.report2 = Report.objects.create(coordinates=cls.parc_coord1,
+                                            intensity=12)
+
+        cls.report3 = Report.objects.create(coordinates=cls.parc_coord3,
+                                            intensity=5)
+
+    def test_complete_coords(self):
+        # existence
+        self.assertIsNotNone(self.full_coord1)
+        self.assertIsNotNone(self.full_coord2)
+        self.assertIsNotNone(self.full_coord3)
+        self.assertIsNotNone(self.full_coord4)
+
+        # lat
+        self.assertLess(self.full_coord1.latitude, 0)
+        self.assertGreater(self.full_coord2.latitude, 0)
+        self.assertLess(self.full_coord3.latitude, 0)
+        self.assertLess(self.full_coord4.latitude, 0)
+
+        # elev
+        self.assertGreater(self.full_coord1.elevation, 0)
+        self.assertGreater(self.full_coord2.elevation, 0)
+        self.assertLess(self.full_coord3.elevation, 0)
+        self.assertEqual(self.full_coord4.elevation, 0)
+
+        # long
+        self.assertLess(self.full_coord1.longitude, 0)
+        self.assertGreater(self.full_coord2.longitude, 0)
+        self.assertGreater(self.full_coord3.longitude, 0)
+        self.assertGreater(self.full_coord4.longitude, 0)
+
+        # str
+        self.assertTrue(str(self.full_coord1.elevation) in str(self.full_coord1))
+        self.assertTrue("m" in str(self.full_coord1))
+
+    def test_incomplete_coords(self):
+        # existence
+        self.assertIsNotNone(self.parc_coord1)
+        self.assertIsNotNone(self.parc_coord2)
+        self.assertIsNotNone(self.parc_coord3)
+        self.assertIsNotNone(self.parc_coord4)
+
+        # lat
+        self.assertLess(self.parc_coord1.latitude, 0)
+        self.assertGreater(self.parc_coord2.latitude, 0)
+        self.assertLess(self.parc_coord3.latitude, 0)
+        self.assertLess(self.parc_coord4.latitude, 0)
+
+        # elev
+        self.assertIsNone(self.parc_coord1.elevation)
+        self.assertIsNone(self.parc_coord2.elevation)
+        self.assertIsNone(self.parc_coord3.elevation)
+        self.assertIsNone(self.parc_coord4.elevation)
+
+        # long
+        self.assertLess(self.parc_coord1.longitude, 0)
+        self.assertGreater(self.parc_coord2.longitude, 0)
+        self.assertGreater(self.parc_coord3.longitude, 0)
+        self.assertGreater(self.parc_coord4.longitude, 0)
+
+        # str
+        self.assertTrue('m' not in str(self.parc_coord1))
+
+    def test_reports(self):
+        self.assertIsNotNone(self.report1)
+        self.assertIsNotNone(self.report2)
+
+        with self.assertRaises(ValidationError):
+            Report.objects.create(coordinates=self.full_coord1, intensity=-1)
+            Report.objects.create(coordinates=self.full_coord1, intensity=13)
+            Report.objects.create(coordinates=self.full_coord1, intensity=-10)
+
+        self.assertIsNotNone(self.report1.timestamp)
+        self.assertIsNotNone(self.report2.timestamp)
+
+        with self.assertRaises(ProtectedError):
+            self.full_coord1.delete()
+            self.parc_coord1.delete()
