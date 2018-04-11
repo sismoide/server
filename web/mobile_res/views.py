6b58baf11abd8ae3e768e0a1e84981d209481f4f
Vichoko <vicenteoyanedel@ing.uchile.cl>
2018-04-11 10:27:06 -0300

    Advances in data model. Added serializers. Added api tests. Configures urls.

--- web/mobile_res/views.py
+++ web/mobile_res/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from mobile_res.models import Report
+from mobile_res.serializers import ReportSerializer
+
+
+class ReportViewSet(mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
