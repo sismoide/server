f4bf245aaf8cb23f82a85c47d68aa3e60a26ef42
rodrigoveraa <rodrigoeveraa@gmail.com>
2018-04-13 17:48:41 -0300

    use getdates directly in date filter

--- web/web_res/views.py
+++ web/web_res/views.py
@@ -10,9 +10,16 @@ import datetime as dt
 # Create your views here.
 
 
-def get_date(request, type, default):
-	date = request.query_params.get(type, default)
+def get_date(request, date_type, default):
+	date = request.query_params.get(date_type, default)
 	return dt.datetime.strptime(date, "%Y-%m-%dT%H:%M")
+
+
+# obtiene las fechas del request
+def getdates(request):
+	start_date = get_date(request, 'start', "1918-01-01T00:00")
+	end_date = get_date(request, 'end', "2100-12-31T23:59")
+	return start_date, end_date
 
 
 class ReportList(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -21,19 +28,9 @@ class ReportList(mixins.ListModelMixin, viewsets.GenericViewSet):
 	serializer_class = IntensitySerializer
 
 	def list(self, request, *args, **kwargs):
-		#start_date = request.query_params.get('start', "2018-01-01T00:00")
-		#end_date = request.query_params.get('end', "2100-12-31T23:59")
-		#start_date = dt.datetime.strptime(start_date, "%Y-%m-%dT%H:%M")
-		#end_date = dt.datetime.strptime(end_date, "%Y-%m-%dT%H:%M")
-		end_date, start_date = self.getdates(request)
-		queryset = self.get_queryset().filter(created_on__range=(start_date, end_date))
+		queryset = self.get_queryset().filter(created_on__range=getdates(request))
 		serializer = IntensitySerializer(queryset, many=True)
 		return Response(serializer.data)
-
-	def getdates(self, request):
-		start_date = get_date(request, 'start', "1918-01-01T00:00")
-		end_date = get_date(request, 'end', "2100-12-31T23:59")
-		return end_date, start_date
 
 
 class EmergencyList(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -42,9 +39,7 @@ class EmergencyList(mixins.ListModelMixin, viewsets.GenericViewSet):
 	serializer_class = EmergencySerializer
 
 	def list(self, request, *args, **kwargs):
-		start_date = get_date(request, 'start', "1918-01-01T00:00")
-		end_date = get_date(request, 'end', "2100-12-31T23:59")
-		queryset = self.get_queryset().filter(report__created_on__range=(start_date, end_date))
+		queryset = self.get_queryset().filter(report__created_on__range=getdates(request))
 		serializer = EmergencySerializer(queryset, many=True)
 		return Response(serializer.data)
 
@@ -55,8 +50,6 @@ class ThreatList(mixins.ListModelMixin, viewsets.GenericViewSet):
 	serializer_class = ThreatSerializer
 
 	def list(self, request, *args, **kwargs):
-		start_date = get_date(request, 'start', "1918-01-01T00:00")
-		end_date = get_date(request, 'end', "2100-12-31T23:59")
-		queryset = self.get_queryset().filter(report__created_on__range=(start_date, end_date))
+		queryset = self.get_queryset().filter(report__created_on__range=getdates(request))
 		serializer = ThreatSerializer(queryset, many=True)
 		return Response(serializer.data)
