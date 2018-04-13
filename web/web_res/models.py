9fabfc001f5d8f162d6ecbfd5c7987d31a208c11
Vichoko <vicenteoyanedel@gmail.com>
2018-04-13 18:00:39 -0300

    Created web user model and tests.

--- web/web_res/models.py
+++ web/web_res/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class WebUserManager(models.Manager):
+
+    def create_web_user(self, name, email=None, password=None):
+        django_user = User.objects.create_user(name, email, password)
+        web_user = WebUser.objects.create(user=django_user)
+        return web_user
+
+
+class WebUser(models.Model):
+    """
+    Django's user has:
+        username
+        password
+        email
+        first_name
+        last_name
+    Django's user is created by:
+        User.objects.create_user(name, email, plain_pass)
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    objects = WebUserManager()
