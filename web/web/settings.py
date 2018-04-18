3cd38767229aeb62695ddfa7ae642292d7382c33
Vichoko <vicenteoyanedel@ing.uchile.cl>
2018-04-18 13:17:08 -0300

    Merge auth into web-auth

--- web/web/settings.py
+++ web/web/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated', todo: habilitar cuando haya forma de autenticar todo
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
--- web/web_res/urls.py
+++ web/web_res/urls.py
@@ -1,20 +1,14 @@
 from django.urls.conf import path
 from rest_framework import routers
-from web_res import views
 from rest_framework.authtoken import views as auth_views
-from django.urls import re_path
+
+from web_res import views
 
 router = routers.DefaultRouter()
 router.register(r'reports', views.ReportList)
 
 urlpatterns = [
     path(r'change_pass', views.WebUserChangePassword.as_view(), name='web_user-change_pass'),
+    path(r'api-token-auth', auth_views.obtain_auth_token, name='web_user-get_token')
 ]
 urlpatterns += router.urls
-
-urlpatterns = [
-    re_path(r'^api-token-auth/', auth_views.obtain_auth_token)
-]
-
-urlpatterns += router.urls
-
