4f1c37546ff5afaaf1635c2ffa6e862dda27b2f8
Vichoko <vicenteoyanedel@ing.uchile.cl>
2018-04-18 13:23:21 -0300

    Merge branch 'develop' of https://github.com/sismoide/server into develop
    # Conflicts:
    #	web/web_res/tests.py

--- web/web_res/serializers.py
+++ web/web_res/serializers.py
@@ -1,46 +1,47 @@
 from rest_framework import serializers
-from mobile_res.models import Report, Coordinates, EmergencyReport, ThreatReport, EmergencyType, ThreatType, EventType
+
+from mobile_res.models import Report, Coordinates, EmergencyReport, ThreatReport, EmergencyType, ThreatType
 
 
 class CoordinatesSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Coordinates
-		fields = ('latitude', 'longitude', 'elevation')
+    class Meta:
+        model = Coordinates
+        fields = ('latitude', 'longitude', 'elevation')
 
 
 class IntensitySerializer(serializers.ModelSerializer):
-	coordinates = CoordinatesSerializer()
+    coordinates = CoordinatesSerializer()
 
-	class Meta:
-		model = Report
-		fields = ('intensity', 'coordinates', 'created_on')
+    class Meta:
+        model = Report
+        fields = ('intensity', 'coordinates', 'created_on')
 
 
 class EmergencyTypeSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = EmergencyType
-		fields = ('title', )
+    class Meta:
+        model = EmergencyType
+        fields = ('title',)
 
 
 class EmergencySerializer(serializers.ModelSerializer):
-	type = EmergencyTypeSerializer()
-	report = IntensitySerializer()
+    type = EmergencyTypeSerializer()
+    report = IntensitySerializer()
 
-	class Meta:
-		model = EmergencyReport
-		fields = ('type', 'report', 'timestamp')
+    class Meta:
+        model = EmergencyReport
+        fields = ('type', 'report', 'timestamp')
 
 
 class ThreatTypeSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = ThreatType
-		fields = ('title', )
+    class Meta:
+        model = ThreatType
+        fields = ('title',)
 
 
 class ThreatSerializer(serializers.ModelSerializer):
-	type = ThreatTypeSerializer()
-	report = IntensitySerializer()
+    type = ThreatTypeSerializer()
+    report = IntensitySerializer()
 
-	class Meta:
-		model = ThreatReport
-		fields = ('type', 'report', 'timestamp')
+    class Meta:
+        model = ThreatReport
+        fields = ('type', 'report', 'timestamp')
--- web/web_res/urls.py
+++ web/web_res/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
-from web_res import views
 
+from web_res import views
 
 router = routers.DefaultRouter()
 router.register(r'reports', views.ReportList)
