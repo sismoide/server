e2f8e8b10d122d169fb75d7fc0835a9f8eeb832d
Vichoko <vicenteoyanedel@gmail.com>
2018-04-18 12:19:26 -0300

    Configurado para permitir solo request con Token. Arreglado modelo de Webuser para que el token se re-genere cuando se cambie algun dato de usuario.
    Agregado serializador y vista para cambiar password.

--- web/web_res/models.py
+++ web/web_res/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
 class WebUserManager(models.Manager):
-
     def create_web_user(self, name, email=None, password=None):
         django_user = User.objects.create_user(name, email, password)
         web_user = WebUser.objects.create(user=django_user)
@@ -23,10 +24,35 @@ class WebUser(models.Model):
         User.objects.create_user(name, email, plain_pass)
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    token = models.TextField(blank=True)
+    token = models.OneToOneField(Token, null=True, blank=True, on_delete=models.SET_NULL)
     objects = WebUserManager()
 
     def save(self, *args, **kwargs):
         if self.token is "" or self.token is None:
             self.token = Token.objects.get_or_create(user=self.user)[0]
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.user)
+
+
+@receiver(pre_save, sender=User)
+def reset_token(sender, instance, **kwargs):
+    """
+    Any time a Django User data is modified, a new token is generated.
+    :param sender:
+    :param instance:
+    :param kwargs:
+    :return: None
+    """
+    try:
+        # try to get web_user of django_user
+        web_user = WebUser.objects.get(user=instance)
+        if web_user.token:
+            # delete old token
+            web_user.token.delete()
+            # generate new one
+            web_user.token = Token.objects.get_or_create(user=instance)[0]
+            web_user.save()
+    except WebUser.DoesNotExist:
+        pass
--- web/web_res/urls.py
+++ web/web_res/urls.py
@@ -1,7 +1,12 @@
+from django.urls.conf import path
 from rest_framework import routers
-from web_res import views
 
+from web_res import views
 
 router = routers.DefaultRouter()
 router.register(r'reports', views.ReportList)
-urlpatterns = router.urls
+
+urlpatterns = [
+    path(r'change_pass', views.WebUserChangePassword.as_view(), name='web_user-change_pass'),
+]
+urlpatterns += router.urls
