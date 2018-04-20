f5601ca5c67e2fe3f6df9e3a3cc1e82b050dc0f3
Vichoko <vicenteoyanedel@gmail.com>
2018-04-20 10:20:21 -0300

    advanceseath

--- web/web_res/views.py
+++ web/web_res/views.py
@@ -1,19 +1,14 @@
 import datetime as dt
 
-from rest_framework import mixins, viewsets
-from rest_framework.response import Response
-
-from mobile_res.models import Report, EmergencyReport, ThreatReport
-from web_res.serializers import IntensitySerializer, EmergencySerializer, ThreatSerializer
-
-
 # Create your views here.
 from rest_framework import mixins, viewsets, permissions, status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
+from mobile_res.models import EmergencyReport, ThreatReport
 from mobile_res.models import Report
 from web_res.models import WebUser
+from web_res.serializers import EmergencySerializer, ThreatSerializer
 from web_res.serializers import IntensitySerializer, WebUserChangePasswordSerializer
 
 
@@ -57,6 +52,10 @@ class ThreatList(mixins.ListModelMixin, viewsets.GenericViewSet):
         queryset = self.get_queryset().filter(report__created_on__range=getdates(request))
         serializer = ThreatSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class NonceViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset =
 
 
 class WebUserChangePassword(GenericAPIView):
