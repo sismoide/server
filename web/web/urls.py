69896435019ad8634a287b77cc12f7f22b4dbaec
Vichoko <vicenteoyanedel@ing.uchile.cl>
2018-04-20 18:04:34 -0300

    Modified authentication: Mobile token cant be used for non-mobile resources. Fixed test for authentication.

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from web.settings import MOBILE_PATH_PREFIX, WEB_PATH_PREFIX
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mobile/', include(('mobile_res.urls', 'mobile_res'), namespace='mobile_res')),
-    path('web/', include(('web_res.urls', 'web_res'), namespace='web_res'))
+    path('{}/'.format(MOBILE_PATH_PREFIX), include(('mobile_res.urls', 'mobile_res'), namespace='mobile_res')),
+    path('{}/'.format(WEB_PATH_PREFIX), include(('web_res.urls', 'web_res'), namespace='web_res'))
 ]
