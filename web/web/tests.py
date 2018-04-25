c6214b52eaa3c78dc64ac54fe2b25b1a9be18741
Vichoko <vicenteoyanedel@ing.uchile.cl>
2018-04-25 13:04:10 -0300

    Added throttle (limit) for Mobile (for Reports and EventReport submissions) and Anonymous (Nonce and Challenge resources) requests. Added tests for Throttle behaviour. Unified random username creation to utils.py Added tests for Nonce/Challenge behaviour. Small fixes.

--- web/web/tests.py
+++ web/web/tests.py
@@ -7,8 +7,8 @@ from rest_framework.test import APITestCase
 
 from mobile_res.models import MobileUser, Report, ThreatType, Coordinates, EmergencyType
 from mobile_res.utils import random_username
-from web.settings import REST_FRAMEWORK
-from web.utils import easy_post
+from web.settings import REST_FRAMEWORK, HASH_CLASS
+from web.utils import easy_post, easy_get
 from web_res.models import WebUser
 
 
@@ -61,16 +61,17 @@ class ThrottleTestCase(APITestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.mobile_limit_rate = int(REST_FRAMEWORK['DEFAULT_THROTTLE_RATES']['mobile'].split("/")[0])
+        cls.reports_limit_rate = int(REST_FRAMEWORK['DEFAULT_THROTTLE_RATES']['reports'].split("/")[0])
+        cls.events_limit_rate = int(REST_FRAMEWORK['DEFAULT_THROTTLE_RATES']['events'].split("/")[0])
+        cls.anon_limit_rate = int(REST_FRAMEWORK['DEFAULT_THROTTLE_RATES']['anon'].split("/")[0])
 
-    def setUp(self):
-        self.web_user = WebUser.objects.create_web_user(random_username())
-        self.mobile_user = MobileUser.objects.create_random_mobile_user()
+        cls.mobile_user = MobileUser.objects.create_random_mobile_user()
+        cls.web_user = WebUser.objects.create_web_user(random_username())
 
     def test_report_throttle(self):
         # mobile token case
         report_url = reverse('mobile_res:report-list')
-        for i in range(self.mobile_limit_rate):
+        for i in range(self.reports_limit_rate):
             # all of theese should pass correct
             data = {'coordinates': {'latitude': 10, 'longitude': 14}}
             response = easy_post(self.client, report_url, data, self.mobile_user.token.key)
@@ -83,39 +84,21 @@ class ThrottleTestCase(APITestCase):
 
     def test_threat_throttle(self):
         # create a report
-        self.mobile_user = MobileUser.objects.create_random_mobile_user()
-        self.mobile_user = MobileUser.objects.create_random_mobile_user()
-
         report = Report.objects.create(coordinates=Coordinates.objects.create(
             latitude=10,
             longitude=22
         )
         )
         threat_url = reverse('mobile_res:threatreport-list')
+        emergency_url = reverse('mobile_res:emergencyreport-list')
 
-        for i in range(self.mobile_limit_rate):
+        for i in range(int(self.reports_limit_rate / 2)):
             t = ThreatType.objects.create(title=random_username())
             data = {'type': t.id, 'report': report.id}
             response = easy_post(self.client, threat_url, data, self.mobile_user.token.key)
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        t = ThreatType.objects.create(title='grietas')
-        data = {'type': t.id, 'report': report.id}
-        response = easy_post(self.client, threat_url, data, self.mobile_user.token.key)
-        self.assertEqual(response.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
-
-    def test_emergency_throttle(self):
-        # create a report
-        self.mobile_user = MobileUser.objects.create_random_mobile_user()
-
-        report = Report.objects.create(coordinates=Coordinates.objects.create(
-            latitude=10,
-            longitude=22
-        )
-        )
-        emergency_url = reverse('mobile_res:emergencyreport-list')
-
-        for i in range(self.mobile_limit_rate):
+        for i in range(int(self.reports_limit_rate / 2)):
             e = EmergencyType.objects.create(title=random_username())
             data = {'type': e.id, 'report': report.id}
             response = easy_post(self.client, emergency_url, data, self.mobile_user.token.key)
@@ -126,4 +109,48 @@ class ThrottleTestCase(APITestCase):
         response = easy_post(self.client, emergency_url, data, self.mobile_user.token.key)
         self.assertEqual(response.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
 
-    # todo: test anon request limit and web request no-limit
+        t = ThreatType.objects.create(title='grietas')
+        data = {'type': t.id, 'report': report.id}
+        response = easy_post(self.client, threat_url, data, self.mobile_user.token.key)
+        self.assertEqual(response.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
+
+    def test_web_resources_unlimit(self):
+        reports_url = reverse('web_res:report-list')
+        emergencies_url = reverse('web_res:emergencyreport-list')
+        threat_url = reverse('web_res:threatreport-list')
+
+        iter_limit = 500
+        for i in range(iter_limit):
+            response = easy_get(self.client, reports_url, {}, self.web_user.token.key)
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        for i in range(iter_limit):
+            response = easy_get(self.client, emergencies_url, {}, self.web_user.token.key)
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        for i in range(iter_limit):
+            response = easy_get(self.client, threat_url, {}, self.web_user.token.key)
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_anon_requests(self):
+        create_nonce_url = reverse("mobile_res:nonce-list")
+        challenge_url = reverse("mobile_res:challenge")
+
+        res = self.client.post(create_nonce_url)
+        self.assertEqual(status.HTTP_201_CREATED, res.status_code)
+        nonce = res.data['key']
+        hash = HASH_CLASS(nonce.encode('utf-8')).hexdigest()
+        res = self.client.post(challenge_url, {"h": hash}, HTTP_AUTHORIZATION=nonce)
+        self.assertEqual(status.HTTP_200_OK, res.status_code)
+
+        for i in range(self.anon_limit_rate - 2):
+            res = self.client.post(create_nonce_url)
+            self.assertEqual(status.HTTP_201_CREATED, res.status_code)
+            nonce = res.data['key']
+
+        hash = HASH_CLASS(nonce.encode('utf-8')).hexdigest()
+        res = self.client.post(challenge_url, {"h": hash}, HTTP_AUTHORIZATION=nonce)
+        self.assertEqual(status.HTTP_429_TOO_MANY_REQUESTS, res.status_code)
+
+        res = self.client.post(create_nonce_url)
+        self.assertEqual(status.HTTP_429_TOO_MANY_REQUESTS, res.status_code)
