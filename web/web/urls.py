b3d386459fb988fe6f1a5cad9fb0f209ea217e18
Vichoko <vicenteoyanedel@gmail.com>
2018-04-30 17:35:03 -0300

    Documentacion

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='docs/docs.html'), name='index'),
     path('admin/', admin.site.urls),
     path('mobile/', include(('mobile_res.urls', 'mobile_res'), namespace='mobile_res')),
     path('web/', include(('web_res.urls', 'web_res'), namespace='web_res'))
