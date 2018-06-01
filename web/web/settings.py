62df5f7dc1bfe82b7742958208286e43497330fe
Vichoko <vicenteoyanedel@gmail.com>
2018-06-01 17:04:18 -0400

    fixed migrations and db setting for master

--- web/web/settings.py
+++ web/web/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i7r2tyl572bx2xa-5(6s*x#pcq-)y9bxk(%ro++lz9)&@t46nj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Truepsycopg2
 
 ALLOWED_HOSTS = ['*']
 
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sismoide',
+        'USER': 'sismoide',
+        'PASSWORD': 'sismoide123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
