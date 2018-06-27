4baf6cdd17919de6fdab53ef5654c77832816813
Vichoko <vicenteoyanedel@gmail.com>
2018-06-27 12:22:15 -0400

    quick fix

--- web/mobile_res/management/commands/import_dbf_landmarks.py
+++ web/mobile_res/management/commands/import_dbf_landmarks.py
@@ -9,31 +9,32 @@ class Command(BaseCommand):
         print('info: starting dbf loader')
 
         file_path = input('ingrese ruta a archivo DBF con edificios o hitos a cargar al sistema: ')
+        table = DBF(file_path, encoding='utf8')
+
         landmark_type = input('ingrese nombre corto del tipo de edificio o hitos: ')
         description = input('ingrese descripcion del tipo de edificio o hitos: ')
-
         type = LandmarkType.objects.get_or_create(name=landmark_type, description=description)[0]
         print('info: loading records to db')
-        with DBF(file_path, encoding='utf8', load=True) as table:
-            for record in table:
-                if landmark_type in record['NOMBRE']:
-                    name = record['NOMBRE']
-                    # remove landmarkType and symbols from name
-                    name = name.replace(type, "")
-                    name = name.replace('-', "")
-                    name = name.strip()
 
-                    address = record['DIRECCION_']
-                    latitude = record['LATITUD_Y_']
-                    longitude = record['LONGITUD_1']
-                    Landmark.objects.create(
-                        name=name,
-                        address=address,
-                        coordinates=Coordinates.objects.get_or_create(
-                            latitude=latitude,
-                            longitude=longitude
-                        )[0],
-                        type=type
-                    )
+        for record in table:
+            if landmark_type in record['NOMBRE']:
+                name = record['NOMBRE']
+                # remove landmarkType and symbols from name
+                name = name.replace(landmark_type, "")
+                name = name.replace('-', "")
+                name = name.strip()
+
+                address = record['DIRECCION_']
+                latitude = record['LATITUD_Y_']
+                longitude = record['LONGITUD_1']
+                Landmark.objects.create(
+                    name=name,
+                    address=address,
+                    coordinates=Coordinates.objects.get_or_create(
+                        latitude=latitude,
+                        longitude=longitude
+                    )[0],
+                    type=type
+                )
         print('info: done loading data into db')
         print('info: thank you for using this tool')
